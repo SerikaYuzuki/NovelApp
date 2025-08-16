eaaefef34bb4891967737ad55809f14b7bbd4e75
Serika Yuzuki <81969251+SerikaYuzuki@users.noreply.github.com>
2025-08-16 18:55:00 +0900

    Initial Commit

---
+++ Novel/NovelApp.swift
@@ -0,0 +1,17 @@
+//
+//  NovelApp.swift
+//  Novel
+//
+//  Created by SerikaYuzuki on 2025/08/16.
+//
+
+import SwiftUI
+
+@main
+struct NovelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NovelTests/NovelTests.swift
@@ -0,0 +1,17 @@
+//
+//  NovelTests.swift
+//  NovelTests
+//
+//  Created by SerikaYuzuki on 2025/08/16.
+//
+
+import Testing
+@testable import Novel
+
+struct NovelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
